4cf8b15f00c52f0bbdb28503e4543cfd221ff85e
ravigupta <rg187888@gmail.com>
2025-04-27 13:03:31 +0530

    add video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,49 +1,50 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true, 
-        index: true // not need or required to write in every field
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // not need or required to write in every field
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    fullname:{
-        type:String,
-        required:true,
-        trim:true,
-        index: true
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
-    avatar:{
-        type: String, //cloudinary URL
-        required: true
+    avatar: {
+      type: String, //cloudinary URL
+      required: true,
     },
-    coverImage:{
-        type: String,
+    coverImage: {
+      type: String,
     },
     watchHistory: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Video"
-        }
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
     ],
     password: {
-        type: String,
-        required: [true, "password is required"]
+      type: String,
+      required: [true, "password is required"],
     },
-    refreshToken:{
-        type: String
-    }
-},
-{timestamps: true} // automatic add => "createdAt date" and "updatedAt date" field 
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true } // automatic add => "createdAt date" and "updatedAt date" field
 );
 
 export const User = mongoose.model("User", userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,1 +1,41 @@
- 
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary URL
+      required: true,
+    },
+    thumbnail: {
+      type: String, // cloudinary URL
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true, // Dependent on you
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
